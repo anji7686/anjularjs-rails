19727e7dc9152e89dadb8eb4653faeaa19105f16
anji7686 <anji7686@gmail.com>
2014-09-29 23:13:12 +0530

    angular js app

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'spring',        group: :development
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
-
+gem 'angular-rails-templates'
 # gem 'unicorn'
 
 # Use Capistrano for deployment
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'receta#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'index'
+  #root '#index'
 end
